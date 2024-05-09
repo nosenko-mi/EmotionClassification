04ef748ff94ea0f9c86100f6b045d0d5459fcdde
Mykola Nosenko <nosenkomi02@gmail.com>
2024-05-09 13:22:57 +0300

    todo: implement new methods

--- app/src/main/java/com/nosenkomi/emotionclassification/record/YamnetClassifierOrig.kt
+++ app/src/main/java/com/nosenkomi/emotionclassification/record/YamnetClassifierOrig.kt
@@ -157,7 +157,15 @@ class YamnetClassifierOrig(
         TODO("Not yet implemented")
     }
 
+    override fun getRecorder(): AudioRecord {
+        TODO("Not yet implemented")
+    }
+
     override fun getState(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSampleRate(): Int {
         TODO("Not yet implemented")
     }
 
