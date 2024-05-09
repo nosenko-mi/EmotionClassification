77565609ee6f50c5ab9559915324d34e4beb29c0
Mykola Nosenko <nosenkomi02@gmail.com>
2024-05-09 13:26:26 +0300

    update hilt providers

--- app/src/main/java/com/nosenkomi/emotionclassification/AppModule.kt
+++ app/src/main/java/com/nosenkomi/emotionclassification/AppModule.kt
@@ -1,27 +1,33 @@
 package com.nosenkomi.emotionclassification
 
 import android.app.Application
+import com.nosenkomi.emotionclassification.classifier.Classifier
 import com.nosenkomi.emotionclassification.classifier.CnnLstmClassifier
-import com.nosenkomi.emotionclassification.classifier.LSTMClassifier
+import com.nosenkomi.emotionclassification.classifier.EmotionClassifier
+import com.nosenkomi.emotionclassification.mlmodel.CnnGruModel
+import com.nosenkomi.emotionclassification.mlmodel.MLModel
+import com.nosenkomi.emotionclassification.mlmodel.YamnetModel
 import com.nosenkomi.emotionclassification.record.AndroidAudioRecorder
-import com.nosenkomi.emotionclassification.record.CustomClassifier
 import com.nosenkomi.emotionclassification.record.AudioRecorder
+import com.nosenkomi.emotionclassification.record.CustomClassifier
 import com.nosenkomi.emotionclassification.record.YamnetClassifier
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import org.tensorflow.lite.support.audio.TensorAudio
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import javax.inject.Singleton
 
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule{
+object AppModule {
 
     @Provides
     @Singleton
     fun provideAndroidAudioRecorder(application: Application): AudioRecorder {
-        return AndroidAudioRecorder(application)
+        return AndroidAudioRecorder(application, sampleRate = 16000)
     }
 
     @Provides
@@ -44,7 +50,43 @@ object AppModule{
 
     @Provides
     @Singleton
-    fun provideMainActivityViewModel(recorder: AudioRecorder, classifier: CnnLstmClassifier): MainActivityViewModel{
+    fun provideBufferMLClassifier(application: Application): MLModel<TensorBuffer> {
+        return CnnGruModel(application)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAudioMLClassifier(application: Application): MLModel<TensorAudio> {
+        return YamnetModel(application)
+    }
+
+    @Provides
+    @Singleton
+    fun provideEmotionClassifier(
+        audioRecorder: AudioRecorder,
+        serModel: MLModel<TensorBuffer>,
+        yamnetModel: MLModel<TensorAudio>
+    ): Classifier {
+        return EmotionClassifier(
+            audioRecorder = audioRecorder,
+            serModel = serModel,
+            yamnetModel = yamnetModel,
+        )
+    }
+
+
+//    @Provides
+//    @Singleton
+//    fun provideMainActivityViewModel(recorder: AudioRecorder, classifier: CnnLstmClassifier): MainActivityViewModel{
+//        return MainActivityViewModel(recorder, classifier)
+//    }
+
+    @Provides
+    @Singleton
+    fun provideMainActivityViewModel(
+        recorder: AudioRecorder,
+        classifier: EmotionClassifier
+    ): MainActivityViewModel {
         return MainActivityViewModel(recorder, classifier)
     }
 }
