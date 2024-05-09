e76945552a5a94b42bf252fa93a7da1673ab5118
Mykola Nosenko <nosenkomi02@gmail.com>
2024-05-09 13:21:59 +0300

    add

---
+++ app/src/main/java/com/nosenkomi/emotionclassification/mlmodel/CnnGruModel.kt
@@ -0,0 +1,40 @@
+package com.nosenkomi.emotionclassification.mlmodel
+
+import android.content.Context
+import com.nosenkomi.emotionclassification.ml.CnnGruV8SeqScaleTranspose16khz
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.label.Category
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+
+
+class CnnGruModel(
+    private val context: Context,
+) : MLModel<TensorBuffer> {
+
+    private var model = CnnGruV8SeqScaleTranspose16khz.newInstance(context)
+    private var isActive = true
+    override fun runInference(input: TensorBuffer): List<Category> {
+        if (!isActive) {
+            create()
+        }
+        return model.process(input).probabilityAsCategoryList
+    }
+
+    override fun getInput(): TensorBuffer {
+        return TensorBuffer.createFixedSize(intArrayOf(1, 64, 64, 1), DataType.FLOAT32)
+    }
+
+    override fun create() {
+        if (isActive) {
+            return
+        }
+        model = CnnGruV8SeqScaleTranspose16khz.newInstance(context)
+        isActive = true
+    }
+
+    override fun destroy() {
+        isActive = false
+        model.close()
+    }
+
+}
