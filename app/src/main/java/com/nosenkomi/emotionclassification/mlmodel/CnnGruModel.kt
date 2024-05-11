169e216c9ae006c6e9fe55bc1214d7e7c9227885
Mykola Nosenko <nosenkomi02@gmail.com>
2024-05-11 14:28:01 +0300

    change model to v9

--- app/src/main/java/com/nosenkomi/emotionclassification/mlmodel/CnnGruModel.kt
+++ app/src/main/java/com/nosenkomi/emotionclassification/mlmodel/CnnGruModel.kt
@@ -1,7 +1,7 @@
 package com.nosenkomi.emotionclassification.mlmodel
 
 import android.content.Context
-import com.nosenkomi.emotionclassification.ml.CnnGruV8SeqScaleTranspose16khz
+import com.nosenkomi.emotionclassification.ml.CnnGruV9SeqScaleTranspose16khz
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.support.label.Category
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
@@ -11,7 +11,7 @@ class CnnGruModel(
     private val context: Context,
 ) : MLModel<TensorBuffer> {
 
-    private lateinit var model: CnnGruV8SeqScaleTranspose16khz
+    private lateinit var model: CnnGruV9SeqScaleTranspose16khz
     private var isActive = false
 
     init {
@@ -32,7 +32,7 @@ class CnnGruModel(
         if (isActive) {
             return
         }
-        model = CnnGruV8SeqScaleTranspose16khz.newInstance(context)
+        model = CnnGruV9SeqScaleTranspose16khz.newInstance(context)
         isActive = true
     }
 
